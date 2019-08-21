db959471010f7ee0310b54d6a5878e6f0fb342e2
Blazej Bucko <blazej.bucko@gmail.com>
2019-08-21 13:26:10 +0200

    Day 1 - part 2

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,36 @@
 use std::fs::File;
 use std::io::Read;
+use std::collections::HashSet;
 
 fn main() -> std::io::Result<()> {
     let mut file = File::open("input/input1.txt")?;
 
     let mut contents = String::new();
-    file.read_to_string(&mut contents);
+    file.read_to_string(&mut contents)?;
 
-    let sum: i32 = contents.lines().map(|str| str.trim().parse::<i32>().unwrap()).sum();
-    println!("{}", sum);
-    Ok(())
+    let mut found_freqs = HashSet::new();
+    let mut current_freq = 0;
+    loop {
+        for freq in contents.lines().map(process_line).into_iter() {
+            current_freq += freq;
+            if !found_freqs.contains(&current_freq) {
+                found_freqs.insert(current_freq.clone());
+            } else {
+                println!("{}", current_freq);
+                return Ok(());
+            }
+        }
+    }
 }
+
+fn process_line(line: &str) -> i32 { line.trim().parse().unwrap() }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_three_ones() {}
+    fn test_three_ones() {
+        assert_eq!(1, process_line("+1"));
+    }
 }
