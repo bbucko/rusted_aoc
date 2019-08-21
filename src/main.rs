09d2ae98de8c2bb9050e53ce3e3876bf5f649924
Blazej Bucko <blazej.bucko@gmail.com>
2019-08-21 13:05:20 +0200

    Day 1 - part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
+use std::fs::File;
+use std::io::Read;
 
+fn main() -> std::io::Result<()> {
+    let mut file = File::open("input/input1.txt")?;
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents);
+
+    let sum: i32 = contents.lines().map(|str| str.trim().parse::<i32>().unwrap()).sum();
+    println!("{}", sum);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_three_ones() {}
 }
