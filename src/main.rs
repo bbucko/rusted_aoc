abbe13f85e25a27eb7279087034eb0003c1b520b
Blazej Bucko <blazej.bucko@gmail.com>
2019-09-19 21:11:41 +0200

    Day 4 - guard who sleeps the most

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,61 @@
+extern crate chrono;
+
+use std::collections::{BTreeSet, HashMap};
 use std::fs::File;
 use std::io::Read;
 
+use chrono::prelude::*;
+
 fn main() -> std::io::Result<()> {
-    let mut file = File::open("input/input2.txt")?;
+    let mut file = File::open("input/input4.txt")?;
 
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    let lines: Vec<&str> = contents
+    let events: BTreeSet<(DateTime<Local>, String)> = contents
         .lines()
+        .map(parse_line)
         .collect();
 
-    let result = lines.clone()
-        .into_iter()
-        .flat_map(|line_a| cartesian_with_line(lines.clone(), line_a))
-        .filter(|(a, b)| only_one_different_character(a, b))
-        .map(|(a, b)| same_characters(a, b))
-        .max()
-        .unwrap();
+    let mut emp_id = 0;
+    let mut start = 0;
+    let mut slept_for = HashMap::new();
 
-    println!("{}", result);
+    for (time, msg) in &events {
+        if msg.starts_with("Guard #") {
+            emp_id = msg
+                .replace("Guard #", "")
+                .replace(" begins shift", "")
+                .parse::<i32>()
+                .unwrap_or(-1);
+        } else if msg.starts_with("falls asleep") {
+            start = time.timestamp_millis();
+        } else if msg.starts_with("wakes up") {
+            let duration = (time.timestamp_millis() - start) / 1000 / 60;
+            println!("Guard #{} wakes up. He slept for: {} mins", emp_id, duration);
+            let slept_for_entry = slept_for.entry(emp_id).or_insert(0);
+            *slept_for_entry += duration;
+
+            start = 0;
+        }
+    }
+
+    let slept_longer = slept_for.iter().max_by(|(_, v1), (_, v2)| v1.cmp(v2)).unwrap();
+    println!("{:?}", slept_longer);
 
     Ok(())
 }
 
-fn cartesian_with_line<'a, 'b>(lines: Vec<&'a str>, line: &'b str) -> Vec<(&'b str, &'a str)> {
-    lines
-        .into_iter()
-        .filter_map(|line_b| {
-            if line.ne(line_b) {
-                Some((line, line_b))
-            } else {
-                None
-            }
-        })
-        .collect()
-}
+fn parse_line(line: &str) -> (DateTime<Local>, String) {
+//    [1518-11-01 00:00] Guard #10 begins shift
+//    [1518-11-01 00:05] falls asleep
+//    [1518-11-01 00:25] wakes up
 
-fn same_characters(line_a: &str, line_b: &str) -> String {
-    line_a
-        .chars()
-        .zip(line_b.chars())
-        .flat_map(|(a, b)| {
-            if a.eq(&b) {
-                Some(a)
-            } else {
-                None
-            }
-        })
-        .collect()
-}
+    let date_as_string = &line[0..18];
+    let msg = &line[19..];
 
-fn only_one_different_character(line_a: &str, line_b: &str) -> bool {
-    line_a
-        .chars()
-        .zip(line_b.chars())
-        .filter(|(char_a, char_b)| char_a.ne(&char_b))
-        .count() == 1
+    let date = Local.datetime_from_str(date_as_string, "[%Y-%m-%d %H:%M]").unwrap();
+    (date, String::from(msg))
 }
 
 #[cfg(test)]
@@ -64,22 +63,5 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_cartesian() {
-        assert_eq!(cartesian_with_line(vec!["b"], "a"), vec![("a", "b")]);
-        assert_eq!(cartesian_with_line(vec!["b", "c"], "a"), vec![("a", "b"), ("a", "c")]);
-        assert_eq!(cartesian_with_line(vec!["a", "b", "c"], "a"), vec![("a", "b"), ("a", "c")]);
-        assert_eq!(cartesian_with_line(vec!["a", "b", "c"], "b"), vec![("b", "a"), ("b", "c")]);
-    }
-
-    #[test]
-    fn test_process_line() {
-        assert_eq!(only_one_different_character("abcdef", "abddef"), true);
-        assert_eq!(only_one_different_character("abcdef", "ijkxyz"), false);
-    }
-
-    #[test]
-    fn test_different() {
-        assert_eq!(same_characters("abcdef", "abddef"), "abdef");
-        assert_eq!(same_characters("abc", "def"), "");
-    }
+    fn test_different() {}
 }
